0b2c3a76616eced3209ff2578466640fa63fbdf4
Luca Benci <benci.luca@gmail.com>
2015-07-04 02:23:01 +0100

    Initial commit

---
+++ Markov/MarkovHelper.cs
@@ -0,0 +1,113 @@
+﻿using WDict = System.Collections.Generic.Dictionary<string, uint>;
+using TDict = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, uint>>;
+
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Markov
+{
+    static public class MarkovHelper
+    {
+        static public TDict BuildTDict(string s, int size)
+        {
+            TDict t = new TDict();
+            string prev = "";
+            foreach (string word in Chunk(s, size) )
+            {
+                if (t.ContainsKey(prev))
+                {
+                    WDict w = t[prev];
+                    if ( w.ContainsKey(word) )
+                        w[word] += 1;
+                    else
+                        w.Add(word, 1);
+                }
+                else
+                    t.Add( prev, new WDict(){{word, 1}} );
+
+                prev = word;
+            }
+
+            return t;
+        }
+
+        static public string[] Chunk(string s, int size)
+        {
+            string[] ls = s.Split(' ');
+            List<string> chunk = new List<string>();
+
+            for (int i = 0; i < ls.Length - size; ++i)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append( ls.Skip(i).Take(size).Aggregate( (w, k) => w + " " + k) );
+                chunk.Add( sb.ToString() );
+            }
+
+            return chunk.ToArray();
+        }
+
+        static public string BuildString(TDict t, int len, bool exact)
+        {
+            string last;
+            List<string> ucStr = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach ( string word in t.Keys.Skip(1) )
+            {
+                if ( char.IsUpper( word.First() ) )
+                    ucStr.Add(word);
+            }
+
+            if (ucStr.Count > 0)
+                sb.Append( ucStr.ElementAt(Program.r.Next(0, ucStr.Count) ) );
+
+            last = sb.ToString();
+            sb.Append(" ");
+
+            WDict w = new WDict();
+ 
+            for (uint i = 0; i < len; ++i)
+            {
+                if (t.ContainsKey(last))
+                    w = t[last];
+                else
+                    w = t[""];
+
+                last = MarkovHelper.Choose(w);
+                sb.Append( last.Split(' ').Last() ).Append(" ");
+            }
+
+            if (!exact)
+            {
+                while (last.Last() != '.')
+                {
+                    if (t.ContainsKey(last))
+                        w = t[last];
+                    else
+                        w = t[""];
+
+                    last = MarkovHelper.Choose(w);
+                    sb.Append(last.Split(' ').Last()).Append(" ");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static private string Choose(WDict w)
+        {
+            long total = w.Sum(t => t.Value);
+
+            while (true)
+            {
+                int i = Program.r.Next(0, w.Count);
+                double c = Program.r.NextDouble();
+                System.Collections.Generic.KeyValuePair<string, uint> k = w.ElementAt(i);
+
+                if ( c < (double)k.Value / total )
+                    return k.Key;
+            }
+        }
+    }
+}
---
+++ Markov/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using WDict = System.Collections.Generic.Dictionary<string, uint>;
+using TDict = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, uint>>;
+
+namespace Markov
+{
+    class Program
+    {
+        static public Random r { get; private set; }
+        static void Main(string[] args)
+        {
+            r = new Random();
+            int limit, size;
+            bool exact = false;
+
+            if (args.Length < 4) { printUsage(); return; }
+            string  inFile = args[0];
+            string outFile = args[1];
+            if (!int.TryParse(args[2], out limit)) { printUsage(); return; }
+            if (!int.TryParse(args[3], out size)) { printUsage(); return; }
+            if (args.Length == 5) exact = true;
+
+            if (!File.Exists(inFile)) { Console.WriteLine("Input file doesn't exist"); return; }
+
+            string s = Regex.Replace(File.ReadAllText(inFile), @"\s+", " ").TrimEnd(' ');
+            TDict t = MarkovHelper.BuildTDict(s, size);
+            File.WriteAllText( outFile, MarkovHelper.BuildString(t, limit, exact).TrimEnd(' ') );
+        }
+
+        static void printUsage()
+        {
+            Console.WriteLine("Usage: Markov.exe infile outfile wordlimit size [exact]");
+        }
+
+    }
+}
